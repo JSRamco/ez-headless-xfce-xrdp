e13a3a4af4f9393da10f6a9f84d9d0a7a815aa08
JSRamco <jsorensen@ramcoprotective.com>
2025-02-27 15:04:51 -0500

    Update ez-headless-xfce-xrdp.sh

--- ez-headless-xfce-xrdp.sh
+++ ez-headless-xfce-xrdp.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
 # Install packages
 echo "Updating packages and installing XRDP, XFCE, Sakura, and Firefox..."
 sudo apt update && sudo apt install -y xrdp xfce4 sakura firefox-esr
-
 # Configure XRDP to use XFCE
 echo "Configuring XRDP to use XFCE..."
 sudo sed -i 's/^exec.*/exec startxfce4/' /etc/xrdp/startwm.sh
-
 # Restart XRDP
 echo "Restarting XRDP service..."
 sudo systemctl restart xrdp
-
 # Completion message
 echo "Setup complete! Connect via RDP to access your XFCE desktop."
 echo "You can manually launch Sakura and Firefox from the XFCE menu."
