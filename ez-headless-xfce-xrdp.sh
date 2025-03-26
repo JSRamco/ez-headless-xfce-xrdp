a220a2ab936ac24b0387cb2fa957e7ae59d544c3
JSRamco <jsorensen@ramcoprotective.com>
2025-03-26 11:58:55 -0400

    Update ez-headless-xfce-xrdp.sh

--- ez-headless-xfce-xrdp.sh
+++ ez-headless-xfce-xrdp.sh
@@ -2,8 +2,8 @@
 # Update and install required packages
 echo "Updating package list and installing XRDP, XFCE, Sakura, and Firefox..."
 sudo apt update
-sudo apt install -y xrdp xfce4 sakura firefox-esr
 # Configure XRDP to use XFCE
+sudo DEBIAN_FRONTEND=noninteractive apt install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" xrdp xfce4 sakura firefox-esr
 echo "Configuring XRDP to use XFCE..."
 sudo sed -i 's/^exec.*/exec startxfce4/' /etc/xrdp/startwm.sh
 # Restart XRDP service
